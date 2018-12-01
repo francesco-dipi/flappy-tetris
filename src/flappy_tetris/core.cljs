385b61b45ada43d0a51003dc9832042501520346
Francesco Dipi <francesco.dipi+github@gmail.com>
2018-12-01 19:29:45 +0100

    Show game messages and minor code refactor
    Shaw game messages at first load and after game over.
    Clean some code.

--- src/flappy_tetris/core.cljs
+++ src/flappy_tetris/core.cljs
@@ -32,7 +32,8 @@
                  :J {:str " .\n .\n.."
                      :sockets [:J :uP :uL :N]}})
 
-(def initial-state {:score 0
+(def initial-state {:score -2
+                    :highscore 0
                     :player-x initial-player-x
                     :player-y (/ (.-innerHeight js/window) 2)})
 
@@ -92,8 +93,6 @@
                                        (repeat ".")
                                        (concat "\n")
                                        clojure.string/join))
-                             ; non threaded version
-                             ;(clojure.string/join (concat (repeat (clojure.string/index-of socket "\n") ".") "\n")))
                              (range (+ 2 wall-blocks)))))))
 
 (defn new-shape
@@ -101,9 +100,7 @@
   []
   (let [tetramino-key (random-key tetraminos)
         tetramino (tetramino-key tetraminos)
-        socket (nth (:sockets tetramino) (rand-int (count (:sockets tetramino))))
-          ;XXX debug
-        _ (print "tetramino: " tetramino "\tsocket: " socket)]
+        socket (nth (:sockets tetramino) (rand-int (count (:sockets tetramino))))]
     (swap! state assoc
       :player-rot 0
       :player-tetramino tetramino-key
@@ -112,15 +109,20 @@
       :wall-str (make-wall-str (get sockets socket))
       :wall-x (.-innerWidth js/window))))
 
-(defn rotate [t] (
-                  clojure.string/join "\n"
-                   (apply map str (reverse (clojure.string/split t "\n")))))
-
-(defn rotate-player [] (swap! state assoc :player-str (rotate (:player-str @state)) :player-rot (mod (inc (:player-rot @state)) 4)))
+(defn rotate [t]
+  (clojure.string/join "\n"
+                  (apply map str (reverse (clojure.string/split t "\n")))))
 
 (defn player-width [] (* block-size (apply max (map count (clojure.string/split (:player-str @state) "\n")))))
 
 (defn rightmost-player-x [] (+ (:player-x @state) (player-width)))
+
+(defn screen-msg [subject & body]
+  [[:fill {:color "lightblue"} [:rect {:x 0 :y 0 :width (.-innerWidth js/window) :height (.-innerHeight js/window)}]]
+   [:fill {:color "#ffffff99"} [:rect {:x 30 :y 100 :width (- (.-innerWidth js/window) 60) :height 300}]]
+   [:fill {:color "black"}
+    [:text {:value subject :x (- (/ (.-innerWidth js/window) 2) (* 6 (count subject))) :y 150 :size 25 :style :bold}]
+    (map-indexed (fn [offset t] [:text {:value t :x 50 :y (+ 200 (* 25 offset)) :size 18 }]) body)]])
 
 (defn dyn-speed
   "dynamic speed, proportional to game's fps"
@@ -137,88 +139,72 @@
     (on-hide [this])
     (on-render [this]
       (p/render game
-        (do 
-          ; move wall towards player
-          (swap! state assoc :wall-x (- (:wall-x @state) (+ (/ (:score @state) 10) (dyn-speed))))
-          ; check if wall touches player
-          (if (and
-                   (= (:player-y @state) (:socket-y @state))
-                   (= (:wall-socket @state)
-                      (-> @state :player-tetramino tetraminos :sockets (nth (:player-rot @state)))))
-            (if (<= (- (:wall-x @state) (:player-x @state)) (/ block-size 2)); TODO use when
-              (do
+        (case (:score @state)
+          -2 (screen-msg
+               "Press any key to start game"
+               "Mouse move to move"
+               "Mouse wheel to rotate"
+               "" ""
+               "Touch on right half to move"
+               "Touch on left half to rotate")
+          -1 (screen-msg
+               (str "GAME OVER - highscore: " (:highscore @state))
+               "Press any key to restart game")
+          (do
+            ; move wall towards player
+            (swap! state assoc :wall-x (- (:wall-x @state) (+ (/ (:score @state) 10) (dyn-speed))))
+            ; check if wall touches player
+            (if (and
+                     (= (:player-y @state) (:socket-y @state))
+                     (= (:wall-socket @state)
+                        (-> @state :player-tetramino tetraminos :sockets (nth (:player-rot @state)))))
+              (when (<= (- (:wall-x @state) (:player-x @state)) (/ block-size 2))
                 (new-shape)
                 (do (.load audio-up) (.play audio-up))
-                (swap! state assoc :score (inc (:score @state)))))
-            (if (< (:wall-x @state) (rightmost-player-x))
-              (swap! state assoc :player-x (- (:wall-x @state) (player-width)))))
-          ; check if player has reach end of screen
-          (if (< (:player-x @state) 0); TODO use when
-            (do
-              (swap! state assoc :score 0 :player-x (/ (.-innerWidth js/window) 3))
+                (swap! state assoc :score (inc (:score @state))))
+              (if (< (:wall-x @state) (rightmost-player-x))
+                (swap! state assoc :player-x (- (:wall-x @state) (player-width)))))
+            ; check if player has reach end of screen
+            (when (< (:player-x @state) 0)
+              (swap! state assoc :highscore (max (:score @state ) (:highscore @state)) :score -1 :player-x (/ (.-innerWidth js/window) 3))
               (do (.load audio-down) (.play audio-down))
-              (new-shape)))
-          ; draw things
-          (concat 
-            [[:fill {:color "lightblue"} [:rect {:x 0 :y 0 :width (.-innerWidth js/window) :height (.-innerHeight js/window)}]]]
-            [[:fill {:color "orange"} (str-to-blocks (:player-str @state) (:player-x @state) (:player-y @state))]]
-            [[:fill {:color "lime"} (str-to-blocks (:wall-str @state) (:wall-x @state) 0)]]
-            ; score
-            [[:fill {:color "green"} [:text {:value (str (:score @state)) :x (- (.-innerWidth js/window) 100) :y 30 :size 30}]]]
-            ;; XXX debug:
-            #_[[:line {:x1 0 :x2 1000 :y1 (:socket-y @state) :y2 (:socket-y @state)}]]
-            [[:fill {:color "black"} [:text {:value (str "state: " @state ) :x 15 :y 15 :size 12}]]]))))))
+              (new-shape))
+            ; draw things
+            (concat 
+              [[:fill {:color "lightblue"} [:rect {:x 0 :y 0 :width (.-innerWidth js/window) :height (.-innerHeight js/window)}]]]
+              [[:fill {:color "orange"} (str-to-blocks (:player-str @state) (:player-x @state) (:player-y @state))]]
+              [[:fill {:color "lime"} (str-to-blocks (:wall-str @state) (:wall-x @state) 0)]]
+              [[:fill {:color "green"} [:text {:value (str "Score: " (:score @state)) :x (- (.-innerWidth js/window) 150) :y 30 :size 30}]]])))))))
 
 
+(defn eventually-reset [] (when (< (:score @state) 0) (swap! state assoc :score 0) (new-shape)))
 
-(comment
-  "keycodes:
-  27: ESC
-  32: space
-  37: left
-  38: up
-  39: rigth
-  40: down")
+(defn rotate-player []
+  (eventually-reset)
+  (swap! state assoc :player-str (rotate (:player-str @state)) :player-rot (mod (inc (:player-rot @state)) 4)))
 
-(comment "event names: https://github.com/google/closure-library/blob/9cddf9f215156940c5e2eb213a4740fd41481907/closure/goog/events/eventtype.js#L51")
-
-(events/listen js/window "keydown"
-  (fn [event]
-    ; TODO: maybe replace case with condp =, so that there is no default case.
-    (case (.-keyCode event)
-      32 (rotate-player)
-      38 (swap! state assoc :player-y (max 0 (- (:player-y @state) movement)))
-      40 (swap! state assoc :player-y (min (-  (.-innerHeight js/window) (* block-size (count (clojure.string/split (:player-str @state) "\n")))) (+ (:player-y @state) movement)))
-      27 (reset! state initial-state)
-      ; W
-      87 (swap! state assoc :wall-str (make-wall-str (random-value sockets)))
-      ;      87 (swap! state assoc :wall-str (make-wall-str (get sockets (nth (keys sockets) (rand-int (count sockets))))))
-      ; N
-      78 (new-shape)
-      ; L
-      76 (print (-> @state :player-tetramino tetraminos :sockets (nth (:player-rot @state))))
-      ((.-log js/console) (.-keyCode event)))))
-
-(events/listen js/window "click" rotate-player)
-(events/listen js/window "wheel" rotate-player) ; TODO: maybe use wheel up/down to rotate clockwise/counterclockwise?
+(defn pointer-move [event]
+    (swap! state assoc :player-y (* block-size (int (/ (.-clientY event) block-size)))))
 
 (defn mouse-move [event]
   ; TODO I'm sure there is a cleaner way to express this... a sort of threading but with the function
   (if (not (goog.object/get (goog.object/get (goog.object/get event "event_") "sourceCapabilities") "firesTouchEvents"))
     (pointer-move event)))
 
-(defn pointer-move [event]
-    (swap! state assoc :player-y (* block-size (int (/ (.-clientY event) block-size)))))
-
 (defn handle-touch [event]
+  (eventually-reset)
   (if (< (.-clientX event) (/ (.-innerWidth js/window) 2))
     (rotate-player)
     (pointer-move event)))
 
 
+(events/listen js/window "wheel" rotate-player) ; TODO: maybe use wheel up/down to rotate clockwise/counterclockwise?
+(events/listen js/window "click" rotate-player)
 (events/listen js/window "mousemove" mouse-move)
 (events/listen js/window "touchmove" handle-touch)
 (events/listen js/window "touchend" handle-touch)
+(events/listen js/window "keydown"
+  (fn [event] (eventually-reset)))
 
 (events/listen js/window "resize"
   (fn [event]
